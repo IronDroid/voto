672c74d99b8e184611129f0c6764e476068ee314
Jhonny Cruz <jhonlimaster@gmail.com>
2013-04-10 23:24:12 -0400

    popup y pagina de resultados

---
+++ voto/static/js/popup.js
@@ -0,0 +1,31 @@
+ jQuery(function(){
+    var popup = new function () {
+        var $self      = $('#pago'),
+            $overlay = $self.find('.overlay'),
+            $panel   = $self.find('.panel');
+        
+        this.hide = function () {
+            $overlay.addClass('fadeOut');
+            $panel.addClass('bounceOutUp');
+
+            setTimeout(function () {
+                $self.removeClass('show');
+                $overlay.removeClass('fadeOut').removeClass('fadeIn');
+                $panel.removeClass('bounceOutUp').removeClass('bounceInDown');
+            }, 1010);
+
+            return false;
+        };
+
+        this.show = function () {
+            $self.addClass('show');
+            $overlay.addClass('fadeIn');
+            $panel.addClass('bounceInDown');
+        };
+
+        // botones para cerrar
+        // $overlay.click(this.hide);
+    }();
+    $('.close').click(popup.hide);
+    $('.info').click(popup.show);
+ });
